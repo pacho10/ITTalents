7c3e26eb17b8d28b846bf17dd3d3e6b38144633f
pacho10 <pmpetkov1983@abv.bg>
2017-10-19 14:31:54 +0300

    add createProject.jsp add CreateProjectServlet.java and methods about crate project functionality in ProjectDAO.java

---
+++ src/main/resources/bg/ittalents/efficientproject/controller/CreateProjectServlet.java
@@ -0,0 +1,72 @@
+package bg.ittalents.efficientproject.controller;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bg.ittalents.efficientproject.model.exception.DBException;
+import bg.ittalents.efficientproject.model.exception.EffPrjDAOException;
+import bg.ittalents.efficientproject.model.interfaces.DAOStorageSourse;
+import bg.ittalents.efficientproject.model.interfaces.IOrganizationDAO;
+import bg.ittalents.efficientproject.model.interfaces.IProjectDAO;
+import bg.ittalents.efficientproject.model.pojo.Organization;
+import bg.ittalents.efficientproject.model.pojo.Project;
+import bg.ittalents.efficientproject.model.pojo.User;
+
+/**
+ * Servlet implementation class CreateCategoryServlet
+ */
+@WebServlet("/CreateProject")
+public class CreateProjectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateProjectServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("./createProject.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String deadline = request.getParameter("deadline");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+		try {
+			Date date = new Date((sdf.parse(deadline)).getTime());
+			User user = (User)request.getSession().getAttribute("user");
+			Organization org = IOrganizationDAO.getDAO(DAOStorageSourse.DATABASE).getOrgById(user.getOrganization().getId());
+			
+			Project projectToAdd = new Project(name, date, org);
+			
+			IProjectDAO.getDAO(DAOStorageSourse.DATABASE).addProject(projectToAdd);
+		} catch (ParseException | EffPrjDAOException | DBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
---
+++ src/main/webapp/createProject.jsp
@@ -0,0 +1,84 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page isELIgnored="false"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Efficient Project</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<link rel="stylesheet" type="text/css"
+	href="bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css"
+	href="font-awesome/css/font-awesome.min.css" />
+
+<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="bootstrap/css/simple-sidebar.css" rel="stylesheet">
+</head>
+<body>
+	<div id="createproject" style="margin-top: 50px;"
+		class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+
+		<div class="panel panel-info">
+			<div class="panel-heading">
+
+				<div class="panel-title">Create New Project</div>
+
+			</div>
+			<div class="panel-body">
+
+				
+				<form method="post" action="./CreateProject" id="create-project"
+					class="form-horizontal" role="form">
+
+					<!--<div id="signupalert" style="display: none"
+						class="alert alert-danger">
+						<p>Error:</p>
+						<span></span>
+					</div>-->
+
+					<div class="form-group">
+						<label for="email" class="col-md-3 control-label">Name</label>
+						<div class="col-md-9">
+							<input type="text" class="form-control" name="name"
+								placeholder="Name" required>
+						</div>
+					</div>
+
+
+					<div class="form-group">
+						<label for="password" class="col-md-3 control-label">Deadline</label>
+						<div class="col-md-9">
+							<input type="date" id="deadline" class="form-control"
+								name="deadline" placeholder="Deadline" required>
+						</div>
+					</div>
+
+					<c:if test="${not empty errorMessage }">
+						<div class="form-group">
+							<div class="col-md-offset-3 col-md-9">
+								<span style="color: red"> <c:out
+										value="${ errorMessage }" />
+								</span>
+							</div>
+						</div>
+					</c:if>
+
+					<div class="form-group">
+						<!-- Button -->
+						<div class="col-md-offset-3 col-md-9">
+							<button id="btn-create-project" type="submit" class="btn btn-info">
+								<i class="icon-hand-right"></i> Create
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>	
+</body>
+</html>
