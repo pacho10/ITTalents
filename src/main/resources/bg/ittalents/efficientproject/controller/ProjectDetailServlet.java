d9c20caeb27e9f13f8beef868af16188551ef9a6
Yana Todorova <qnince_to@abv.bg>
2017-11-05 09:48:40 +0200

    projectdatailservlet added validations

--- src/main/resources/bg/ittalents/efficientproject/controller/ProjectDetailServlet.java
+++ src/main/resources/bg/ittalents/efficientproject/controller/ProjectDetailServlet.java
@@ -30,18 +30,8 @@ import bg.ittalents.efficientproject.model.pojo.User;
 @WebServlet("/projectdetail")
 public class ProjectDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final DAOStorageSourse SOURCE_DATABASE = DAOStorageSourse.DATABASE;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public ProjectDetailServlet() {
-		super();
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 		try {
 
@@ -49,29 +39,45 @@ public class ProjectDetailServlet extends HttpServlet {
 				response.sendRedirect("/LogIn");
 				return;
 			}
-			
-			int projectId = Integer.parseInt(request.getParameter("projectId"));
+			if (request.getParameter("projectId") != null) {
+				User user = (User) request.getSession().getAttribute("user");
 
-			Sprint currentSprint = ISprintDAO.getDAO(DAOStorageSourse.DATABASE).getCurrentSprint(projectId);
-			if (currentSprint != null) {
-				request.setAttribute("currentSprint", currentSprint);
+				if (!user.isAdmin()) {
+					request.getRequestDispatcher("errorNotAuthorized.jsp").forward(request, response);
+					return;
+				}
+				int projectId = Integer.parseInt(request.getParameter("projectId"));
+				/**
+				 * check if the project is of this admin
+				 */
+				if (!IProjectDAO.getDAO(SOURCE_DATABASE).isThisProjectOfThisUser(projectId, user.getId())) {
+					request.getRequestDispatcher("errorNotAuthorized.jsp").forward(request, response);
+					return;
+				}
+				Sprint currentSprint = ISprintDAO.getDAO(DAOStorageSourse.DATABASE).getCurrentSprint(projectId);
+				if (currentSprint != null) {
+					request.setAttribute("currentSprint", currentSprint);
+				}
+
+				Project currentProject = IProjectDAO.getDAO(DAOStorageSourse.DATABASE).getProjectByID(projectId);
+				request.setAttribute("project", currentProject);
+
+				List<Epic> epics = IEpicDAO.getDAO(DAOStorageSourse.DATABASE).getAllEpicsByProject(projectId);
+				request.setAttribute("epics", epics);
+
+				List<User> users = IProjectDAO.getDAO(DAOStorageSourse.DATABASE)
+						.getAllWorkersWorkingOnAProject(projectId);
+				request.setAttribute("workers", users);
+
+				// get current sprintby id---->if there is no current sprint--->message and
+				// redirect to create new one
+				// if there is current sprnt--->show the sprint
+
+				RequestDispatcher rd = request.getRequestDispatcher("/projectDetail.jsp");
+				rd.forward(request, response);
+			} else {
+				request.getRequestDispatcher("error2.jsp").forward(request, response);
 			}
-
-			Project currentProject = IProjectDAO.getDAO(DAOStorageSourse.DATABASE).getProjectByID(projectId);
-			request.setAttribute("project", currentProject);
-
-			List<Epic> epics = IEpicDAO.getDAO(DAOStorageSourse.DATABASE).getAllEpicsByProject(projectId);
-			request.setAttribute("epics", epics);
-
-			List<User> users = IProjectDAO.getDAO(DAOStorageSourse.DATABASE).getAllWorkersWorkingOnAProject(projectId);
-			request.setAttribute("workers", users);
-
-			// get current sprintby id---->if there is no current sprint--->message and
-			// redirect to create new one
-			// if there is current sprnt--->show the sprint
-
-			RequestDispatcher rd = request.getRequestDispatcher("/projectDetail.jsp");
-			rd.forward(request, response);
 		} catch (DBException | EffPrjDAOException | IOException | ServletException e) {
 			try {
 				response.sendRedirect("error.jsp");
@@ -81,6 +87,5 @@ public class ProjectDetailServlet extends HttpServlet {
 			}
 		}
 	}
-
 
 }
